41d0430d194cf1141c4951aa703f98fd90165ce4
Alexandra Debartolo <alexandra.debartolo@baruchmail.cuny.edu>
2021-12-06 21:14:10 +0000

    fixing dates

--- models/Unsanitary_Road_View/unsanitary_complaint_dim.sql
+++ models/Unsanitary_Road_View/unsanitary_complaint_dim.sql
@@ -1,11 +1,13 @@
 SELECT
 {{ dbt_utils.surrogate_key(['complaint_type']) }} AS complaint_dim_id,
+FORMAT_DATE("%Y%m%d", CAST(created_date as date)) as unsanitary_complaint_date,
 complaint_type,
 descriptor 
 FROM
 ( SELECT DISTINCT 
 complaint_type,
-descriptor
+descriptor, SUBSTRING(CAST(created_date as string), 1, 10) AS created_date
 FROM `bigquery-public-data.new_york_311.311_service_requests`
-where complaint_type = 'UNSANITARY CONDITION' OR complaint_type = 'Dirty Conditions' OR complaint_type = 'Dirty Condition' 
+where created_date >= '2018-01-01' and complaint_type = 'UNSANITARY CONDITION' OR complaint_type = 'Dirty Conditions' 
+OR complaint_type = 'Dirty Condition' 
 )
--- models/core/location_dim.sql
+++ models/core/location_dim.sql
@@ -1,12 +1,13 @@
 SELECT DISTINCT
 {{ dbt_utils.surrogate_key(['incident_zip', 'borough', 'location_type']) }} AS location_dim_id, 
+FORMAT_DATE("%Y%m%d", CAST(created_date as date)) as created_date,
 incident_zip, 
 borough, 
 location_type,
 latitude, 
 longitude, 
 FROM (
-    SELECT DISTINCT Incident_zip, borough, Location_type, Latitude, Longitude 
+    SELECT DISTINCT SUBSTRING(CAST(created_date as string), 1, 10) AS created_date, Incident_zip, borough, Location_type, Latitude, Longitude 
     FROM `bigquery-public-data.new_york_311.311_service_requests` 
     WHERE complaint_type IN ('UNSANITARY CONDITION','Dirty Conditions', 'Dirty Condition', 'Rodent')
 ) 
